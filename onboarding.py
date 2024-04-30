8479711cd382b1fb7e6ff9bf34b318507e18216e
kellyDMmendes <kellymendes169@gmail.com>
2024-04-30 11:25:17 -0300

    Update onboarding.py

---
+++ onboarding.py
@@ -0,0 +1,11 @@
+''' Your program must destroy the enemy ships by shooting the closest enemy on each turn. '''
+while 1:
+    enemy_1 = input()
+    dist_1 = int(input())
+    enemy_2 = input()
+    dist_2 = int(input())
+
+    if dist_1 < dist_2:
+        print(enemy_1)
+    else:
+        print(enemy_2)
